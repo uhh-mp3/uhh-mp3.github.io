97fa758ceb8d44c7cbf1ab3ee9966e5e5e523439
uhh-mp3 <74452178+uhh-mp3@users.noreply.github.com>
2021-09-25 15:45:40 +0300

    Add files via upload

---
+++ t1/JavaScript.js
@@ -0,0 +1,24 @@
+let score = 0;
+let test1 = prompt(`Вам є 18 років?`);
+if(test1 == Так || نعم){
+score = score+1;
+}else{
+}
+let test2 = prompt(`48562 : 85 = 571,3176470588234?`);
+if(test2 == Неправильно || لا) {
+    score = score+1;
+    }else{
+    }
+let test3 = prompt(`Правильно? Падіння Західної Римської імперії відбулося в 476 році?`);
+if(test3 == Правильно || حق){
+    score = score+1;
+    }else{
+    }
+ let test4 = prompt(`Кіт вміє хрюкати?`);
+ if(test4 == Ні || لا){
+    score = score+1;
+    }else{
+    }
+  console.log(`Вітаю ви набрали ${score} бали`)
+
+ 
